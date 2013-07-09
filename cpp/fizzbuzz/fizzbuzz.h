a1e1e9085e3b4de0abbb7f5bc36346c83cb00733
datsuns <the.skeleton7@gmail.com>
2013-07-09 12:54:58 +0900

    ざっとだけ作成

---
+++ cpp/fizzbuzz/fizzbuzz.h
@@ -0,0 +1,10 @@
+#ifndef SSC_PROGRAMMING_CONTEST_CPP_FIZZBUZZ_H_INCLUDED
+#define SSC_PROGRAMMING_CONTEST_CPP_FIZZBUZZ_H_INCLUDED
+
+#include <string>
+
+const std::string& fizzbuzz(int n);
+
+
+#endif
+
--- cpp/fizzbuzz/main.cpp
+++ cpp/fizzbuzz/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <cassert>
+#include "fizzbuzz.h"
 
-using namespace st;
+using namespace std;
 
-int main( int argc, char* argv[] )
-{
+void test(){
+  for(int i = 1; i < 100; i++){
+    string s = "";
+    if(i % 3 == 0){
+      s += "fizz";
+    }
+    if(i % 5 == 0){
+      s += "buzz";
+    }
+    const string& ret = fizzbuzz(i);
+    cout << i << " : " << ret << endl;
+    assert( ret == s );
+  }
+}
+
+void time_calculate(){
+}
+
+int main( int argc, char* argv[] ){
+  test();
+  time_calculate();
   return 0;
 }
