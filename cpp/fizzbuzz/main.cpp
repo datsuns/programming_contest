ca29ac98b3625b5a80b320e0c4bf2353cb2681e5
datsuns <the.skeleton7@gmail.com>
2013-07-11 00:57:13 +0900

    I/F仕様を更新

--- cpp/fizzbuzz/main.cpp
+++ cpp/fizzbuzz/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <cassert>
 #include <ctime>
+#include <algorithm>
+#include <vector>
+
+
 #include "fizzbuzz.h"
 
 using namespace std;
@@ -11,11 +15,13 @@ void test_divide_with_3(){
     assert( fizzbuzz(123 * 3) == "fizz" );
 }
 
+
 void test_divide_with_5(){
     assert( fizzbuzz(5) == "buzz" );
     assert( fizzbuzz(20) == "buzz" );
     assert( fizzbuzz(100 * 5) == "buzz" );
 }
+
 
 void test_divide_with_15(){
     assert( fizzbuzz(15) == "fizzbuzz" );
@@ -23,27 +29,44 @@ void test_divide_with_15(){
     assert( fizzbuzz(123 * 15) == "fizzbuzz" );
 }
 
+
 void test_divide_with_other(){
     assert( fizzbuzz(1) == "1" );
     assert( fizzbuzz(23) == "23" );
     assert( fizzbuzz(1198) == "1198" );
 }
 
+
 void test(){
   test_divide_with_3();
   test_divide_with_5();
   test_divide_with_15();
   test_divide_with_other();
+  cout << "OK." << endl;
 }
 
-void calculate_time(){
+
+float calculate_once(){
   auto start = clock();
-  for(int i = 0; i < 1000000; i++){
+  vector<int> range(1000000);
+  iota(range.begin(), range.end(), 1);
+  for(auto i : range){
     fizzbuzz(i);
   }
   auto end = clock();
-  cout << (float)(end - start) / CLOCKS_PER_SEC << " sec." << endl;
+  return (float)(end - start) / CLOCKS_PER_SEC;
 }
+
+
+void calculate_time(){
+  const int times = 5;
+  float result = 0;
+  for(int i = 0; i < times; i++){
+    result += calculate_once();
+  }
+  cout << result / times << " sec." << endl;
+}
+
 
 int main( int argc, char* argv[] ){
   if(argc == 1){
