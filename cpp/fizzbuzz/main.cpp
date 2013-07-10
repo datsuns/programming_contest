54a60b2eef63ba0f06d6931494bd9d745e74a89e
datsuns <the.skeleton7@gmail.com>
2013-07-11 00:10:58 +0900

    機能をひと通り作成
    ビルド方法修正
    時間計算処理追加

--- cpp/fizzbuzz/main.cpp
+++ cpp/fizzbuzz/main.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
 #include <cassert>
+#include <ctime>
 #include "fizzbuzz.h"
 
 using namespace std;
 
 void test(){
   for(int i = 1; i < 100; i++){
-    string s = "";
-    if(i % 3 == 0){
-      s += "fizz";
+    string expected = "";
+    if(i % 15 == 0){
+      expected = "fizzbuzz";
     }
-    if(i % 5 == 0){
-      s += "buzz";
+    else if(i % 5 == 0){
+      expected = "buzz";
     }
-    const string& ret = fizzbuzz(i);
+    else if(i % 3 == 0){
+      expected = "fizz";
+    }
+    else{
+      char s[10];
+      sprintf(s, "%d", i);
+      expected = s;
+    }
+
+    auto ret = fizzbuzz(i);
     cout << i << " : " << ret << endl;
-    assert( ret == s );
+    assert( ret == expected );
   }
 }
 
 void time_calculate(){
+  auto start = clock();
+  for(int i = 0; i < 1000000; i++){
+    fizzbuzz(i);
+  }
+  auto end = clock();
+  cout << (float)(end - start) / CLOCKS_PER_SEC << " sec." << endl;
 }
 
 int main( int argc, char* argv[] ){
-  test();
-  time_calculate();
+  if(argc == 1){
+    test();
+  }
+  else{
+    time_calculate();
+  }
   return 0;
 }
