fe5249c80796d1b59e30f0b78596aea1beb10025
datsuns <the.skeleton7@gmail.com>
2013-07-11 00:31:20 +0900

    sampleを追加

--- cpp/fizzbuzz/main.cpp
+++ cpp/fizzbuzz/main.cpp
@@ -5,31 +5,38 @@
 
 using namespace std;
 
-void test(){
-  for(int i = 1; i < 100; i++){
-    string expected = "";
-    if(i % 15 == 0){
-      expected = "fizzbuzz";
-    }
-    else if(i % 5 == 0){
-      expected = "buzz";
-    }
-    else if(i % 3 == 0){
-      expected = "fizz";
-    }
-    else{
-      char s[10];
-      sprintf(s, "%d", i);
-      expected = s;
-    }
-
-    auto ret = fizzbuzz(i);
-    cout << i << " : " << ret << endl;
-    assert( ret == expected );
-  }
+void test_divide_with_3(){
+    assert( fizzbuzz(3) == "fizz" );
+    assert( fizzbuzz(9) == "fizz" );
+    assert( fizzbuzz(123 * 3) == "fizz" );
 }
 
-void time_calculate(){
+void test_divide_with_5(){
+    assert( fizzbuzz(5) == "buzz" );
+    assert( fizzbuzz(20) == "buzz" );
+    assert( fizzbuzz(100 * 5) == "buzz" );
+}
+
+void test_divide_with_15(){
+    assert( fizzbuzz(15) == "fizzbuzz" );
+    assert( fizzbuzz(45) == "fizzbuzz" );
+    assert( fizzbuzz(123 * 15) == "fizzbuzz" );
+}
+
+void test_divide_with_other(){
+    assert( fizzbuzz(1) == "1" );
+    assert( fizzbuzz(23) == "23" );
+    assert( fizzbuzz(1198) == "1198" );
+}
+
+void test(){
+  test_divide_with_3();
+  test_divide_with_5();
+  test_divide_with_15();
+  test_divide_with_other();
+}
+
+void calculate_time(){
   auto start = clock();
   for(int i = 0; i < 1000000; i++){
     fizzbuzz(i);
@@ -43,7 +50,8 @@ int main( int argc, char* argv[] ){
     test();
   }
   else{
-    time_calculate();
+    calculate_time();
   }
   return 0;
 }
+
